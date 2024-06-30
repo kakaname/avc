d9906863dd34aa6956b8358eb7c609a6ee14d64d
anxiety <youngwo2@ualberta.ca>
2024-06-30 09:24:24 -0600

    added creating more directories

--- src/initalize.rs
+++ src/initalize.rs
@@ -8,6 +8,22 @@ pub fn initalize() {
   Ok(_) => println!("repository created"),
   Err(e) => println!("failed to create repository: {}", e),
   }
+  match fs::create_dir_all("./.avc/blob"){
+  Ok(_) => (),
+  Err(e) => println!("failed to create repository: {}", e),
+  }
+  match fs::create_dir_all("./.avc/tag"){
+  Ok(_) => (),
+  Err(e) => println!("failed to create repository: {}", e),
+  }
+  match fs::create_dir_all("./.avc/name"){
+  Ok(_) => println!("repository created"),
+  Err(e) => println!("failed to create repository: {}", e),
+  }
+  match fs::create_dir_all("./.avc/message"){
+  Ok(_) => println!("repository created"),
+  Err(e) => println!("failed to create repository: {}", e),
+  }
 
   return;
   
